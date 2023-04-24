a2409ea88d955cf40e87f57b23aae331906cab06
2005anurag2602 <113921508+2005anurag2602@users.noreply.github.com>
2023-04-24 20:35:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '53e1c7e0bamsh602457e0c73689bp12d90ejsn2e8b41e7ef27',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+const getWeather = (city)=>{
+cityName.innerHTML= city
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' +city, options)
+	.then(response => response.json())
+	.then(response => {
+		console.log(response)
+		cloud_pct.innerHTML = response.cloud_pct
+		feels_like.innerHTML = response.feels_like
+		humidity.innerHTML = response.humidity
+		humidity2.innerHTML = response.humidity
+		max_temp.innerHTML = response.max_temp
+		min_temp.innerHTML = response.min_temp
+		sunrise.innerHTML = response.sunrise
+		sunset.innerHTML = response.sunset
+		temp.innerHTML = response.temp
+		temp2.innerHTML = response.temp
+		wind_degrees.innerHTML = response.wind_degrees
+		wind_speed.innerHTML = response.wind_speed
+		wind_speed2.innerHTML = response.wind_speed
+
+
+
+	})
+	.catch(err => console.error(err));
+}
+submit.addEventListener("click",(e)=>{
+	e.preventDefault()
+	getWeather(city.value)
+})
